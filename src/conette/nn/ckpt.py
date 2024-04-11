7c113c5cf02611fbc43117b2c325282fb1773428
Labbeti <etienne.labbe31@gmail.com>
2024-04-11 11:31:36 +0200

    Mod: Update CNext ckpts names.

--- src/conette/nn/ckpt.py
+++ src/conette/nn/ckpt.py
@@ -7,17 +7,23 @@ from torchoutil.utils.ckpt import ModelCheckpointRegister
 # Hash type : md5
 CNEXT_REGISTER = ModelCheckpointRegister(
     infos={
-        "cnext_bl": {
-            "architecture": "ConvNeXt",
-            "url": "https://zenodo.org/record/8020843/files/convnext_tiny_465mAP_BL_AC_70kit.pth?download=1",
-            "hash": "0688ae503f5893be0b6b71cb92f8b428",
-            "fname": "convnext_tiny_465mAP_BL_AC_70kit.pth",
-        },
         "cnext_nobl": {
             "architecture": "ConvNeXt",
             "url": "https://zenodo.org/record/8020843/files/convnext_tiny_471mAP.pth?download=1",
             "hash": "e069ecd1c7b880268331119521c549f2",
             "fname": "convnext_tiny_471mAP.pth",
+        },
+        "cnext_bl_70": {
+            "architecture": "ConvNeXt",
+            "url": "https://zenodo.org/record/8020843/files/convnext_tiny_465mAP_BL_AC_70kit.pth?download=1",
+            "hash": "0688ae503f5893be0b6b71cb92f8b428",
+            "fname": "convnext_tiny_465mAP_BL_AC_70kit.pth",
+        },
+        "cnext_bl_75": {
+            "architecture": "ConvNeXt",
+            "url": "TODO",
+            "hash": "TODO",
+            "fname": "convnext_tiny_465mAP_BL_AC_75kit.pth",
         },
     },
     state_dict_key="model",
